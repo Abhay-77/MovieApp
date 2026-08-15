fb4a7c6c4c4ca635ff844dc24f4ae11fa7f60b54
Abhay Murali M <abhaymuralim9@gmail.com>
2026-08-16 00:40:49 +0530

    implemented home page

--- lib/api.dart
+++ lib/api.dart
@@ -2,9 +2,9 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:convert';
 
-Future<List<dynamic>> getPopularMovies() async {
-  final token = dotenv.env['TMDB_API_TOKEN'];
+final token = dotenv.env['TMDB_API_TOKEN'];
 
+Future<List<dynamic>> getPopularMovies() async {
   final response = await http.get(
     Uri.parse('https://api.themoviedb.org/3/movie/popular'),
     headers: {
@@ -15,7 +15,7 @@ Future<List<dynamic>> getPopularMovies() async {
 
   if (response.statusCode == 200) {
     final data = json.decode(response.body);
-    return data['results']; // list of movies
+    return data['results'];
   } else {
     throw Exception('Failed to load movies');
   }
--- lib/pages/movies_page.dart
+++ lib/pages/movies_page.dart
@@ -12,6 +12,7 @@ class _MoviesPageState extends State<MoviesPage> {
   List<dynamic> movies = [];
   bool isLoading = true;
   String? errorMessage;
+  String posterBaseUrl = 'https://image.tmdb.org/t/p/w500';
 
   @override
   void initState() {
@@ -159,7 +160,7 @@ class _MoviesPageState extends State<MoviesPage> {
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(10),
                             child: Image.network(
-                              movie['poster'],
+                              posterBaseUrl + movie['poster_path'],
                               fit: BoxFit.cover,
                               width: double.infinity,
                             ),
@@ -173,7 +174,7 @@ class _MoviesPageState extends State<MoviesPage> {
                           style: const TextStyle(fontWeight: FontWeight.bold),
                         ),
                         Text(
-                          '${movie['year']} • ⭐ ${movie['rating']}',
+                          '${movie['release_date']} • ⭐ ${movie['vote_count']}',
                           style: TextStyle(
                             color: Colors.grey[600],
                             fontSize: 13,
